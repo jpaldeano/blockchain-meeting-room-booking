a33511674f8e77c37320feff25df01ab8b0b3ca5
"jpaldeano" <"jotaldeano@gmail.com">
2022-06-28 13:07:40 +0100

    added createRoom and book functions

--- contracts/Calendar.sol
+++ contracts/Calendar.sol
@@ -11,39 +11,31 @@ contract MeetingRoomBooking {
     struct Room {
         uint id;
         string name; 
-        Booking[10] bookings; // bookings slots , where index 0 represents 9AM-10AM and index 9 represents 4PM-5PM                            
     }
 
     struct Booking {
-        address booker;  // address of who booked the meeting room
-        bytes32 name;   // short name for booking (up to 32 bytes)
+        address booker;   // address of who booked the meeting room
+        bytes32 name;     // short name for booking (up to 32 bytes)
     }
 
-    Room[] public availableRooms;
-
-
-    /** 
-     * @dev Create a new meeting rooms system.
-     * @param roomNames names of the rooms
-     */
-    constructor(string[] memory roomNames) {
-        for (uint i = 0; i < roomNames.length; i++) {
-            Booking b = new Booking[](10);
-            Room memory room = Room(i, roomNames[i], b);
-        }
+    mapping(uint => Booking[10]) public bookings;
+    Room[] public rooms;
+    uint numRooms;
+    address admin;
+    constructor () {
+        admin = msg.sender; // contract creator will be set to admin
     }
 
-    function book(uint roomID, uint slot) public {
-        bool found = false;
-        for (uint i = 0; i < availableRooms.length; i++) {
-            if (availableRooms[i].id == roomID) {
-                found == true;
-                 require(slot >= 0 && slot < 10, "slot should be [0:10[");
-                 require(availableRooms[i].bookings[slot].booker == address(0), "room not available");
+    function createRoom(string memory _name) public {
+         require(
+            msg.sender == admin,
+            "Only contract admin can create rooms."
+        );
+        rooms.push(Room(numRooms, _name));
+    }
 
-                 availableRooms[i].bookings[slot].booker = msg.sender;
-                return;
-            }
-        } 
+    function book(uint _roomID, uint _slot, bytes32 _description) public {
+        Booking memory b1 = Booking(msg.sender, _description);
+        bookings[_roomID][_slot] = b1;
     }
 }
